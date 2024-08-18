eca43a20cf5750d09a3cdbb202bfea43a3a696e9
HamsterKombats <masalauniverses@gmail.com>
2024-08-18 16:50:17 +0545

    Update script.js

--- script.js
+++ script.js
@@ -23,6 +23,16 @@ document.addEventListener('DOMContentLoaded', () => {
             appToken: '82647f43-3f87-402d-88dd-09a90025313f',
             promoId: 'c4480ac7-e178-4973-8061-9ed5b2e17954',
         }
+        5: {
+            name: 'MergeAway',
+            appToken: '8d1cc2ad-e097-4b86-90ef-7a27e19fb833',
+            promoId: 'dc128d28-c45b-411c-98ff-ac7726fbaea4',
+        }
+        6: {
+            name: 'Twerk Race 3D',
+            appToken: '61308365-9d16-4040-8bb0-2f4a4c69074c',
+            promoId: '61308365-9d16-4040-8bb0-2f4a4c69074c',    
+        }
     };
 
     const startBtn = document.getElementById('startBtn');
